5da6caedaddc4e423513b807947663a76caa9f39
PikachuTW <chenyenting0830@gmail.com>
2024-05-23 02:01:11 +0800

    update

--- index.js
+++ index.js
@@ -10,9 +10,25 @@ const client = new Discord.Client({ intents: 131071, partials: ['CHANNEL', 'USER
 
 client.on('ready', () => {
     console.log(`${client.user.tag}, 成員數: ${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)} ，伺服器數: ${client.guilds.cache.size}`);
-    const activity = () => client.user.setActivity(`我是超級可愛的二次元機器人喵！ || Made By Tails`, { type: Discord.ActivityType.Playing });
+    const activity = () => {
+        client.user.setActivity(`我是超級可愛的二次元機器人喵！ || Made By Tails`, { type: Discord.ActivityType.Playing });
+    }
     activity();
     setInterval(activity, 600000);
+    const setAvatar = async () => {
+        const { data } = await axios.get(`${url}/waifu`, {
+            headers: {
+                Authorization: process.env['WAIFU-TOKEN'],
+            }
+        });
+        client.user.setAvatar(data.image.large);
+        client.channels.cache.get('1200376695119695934').send({
+            content: '我換頭像了喵！',
+            files: [data.image.large]
+        })
+    };
+    setAvatar();
+    setInterval(setAvatar, 60 * 60 * 1000);
 });
 
 client.on('messageCreate', async (message) => {
@@ -32,13 +48,33 @@ client.on('messageCreate', async (message) => {
                 name: args.join(' '),
             };
         };
-        const { data } = await axios.get(`${url}/waifu`, config);
-        message.channel.send({
-            embeds: [new Discord.EmbedBuilder()
-                .setTitle(`${data.name.native} ${data.name.full}`)
-                .setImage(data.image.large)
-                .setColor('Random')]
-        });
+        try {
+            const { data } = await axios.get(`${url}/waifu`, config);
+            console.log(data);
+            message.channel.send({
+                embeds: [new Discord.EmbedBuilder()
+                    .setTitle(`${data.name.native} ${data.name.full}`)
+                    .setImage(data.image.large)
+                    .setColor('Random')]
+            });
+        } catch {
+            try {
+                config.params = {
+                    name: null,
+                    anime: args.join(' '),
+                }
+                const { data } = await axios.get(`${url}/waifu`, config);
+                console.log(data);
+                message.channel.send({
+                    embeds: [new Discord.EmbedBuilder()
+                        .setTitle(`${data.name.native} ${data.name.full}`)
+                        .setImage(data.image.large)
+                        .setColor('Random')]
+                });
+            } catch {
+                message.channel.send('找不到這個角色喵！');
+            }
+        }
     }
 });
 
